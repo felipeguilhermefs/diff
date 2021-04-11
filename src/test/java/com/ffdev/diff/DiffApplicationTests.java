158e76247ddb890599fca49ef50bd97dddc6ac67
Felipe Flores <felipeguilhermefs@gmail.com>
2021-04-11 09:40:50 -0300

    Initial commit

---
+++ src/test/java/com/ffdev/diff/DiffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffdev.diff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
