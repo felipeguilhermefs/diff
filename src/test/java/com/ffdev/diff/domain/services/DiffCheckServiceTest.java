cd6c639ef561051ed5cfa545ab0b0aed870d0862
Felipe Flores <felipeguilhermefs@gmail.com>
2021-04-13 16:45:41 -0300

    Improve tests in diff check

--- src/test/java/com/ffdev/diff/domain/services/DiffCheckServiceTest.java
+++ src/test/java/com/ffdev/diff/domain/services/DiffCheckServiceTest.java
@@ -7,6 +7,14 @@ import static com.ffdev.diff.api.enums.DiffResult.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * {@link DiffServiceTest} Unit tests diff algorithm logic.
+ *
+ * <p>Not every tests case here is tested at API level as integration tests.
+ * More granular tests are more useful in this case.
+ *
+ * <p>Random test cases where not used here as it would make the tests more difficult to visualize.
+ */
 @DisplayName("Diff Check Service")
 class DiffCheckServiceTest {
 
@@ -34,62 +42,96 @@ class DiffCheckServiceTest {
     }
 
     @Test
-    @DisplayName("should return differences at the start")
-    public void shouldReturnChangesAtStart() {
-        var result = service.getDiff("some-data", "cene-data");
+    @DisplayName("should return difference at the start")
+    public void shouldReturnDifferenceAtStart() {
+        var lData = "some-data";
+        //           |||
+        var rData = "cene-data";
+        //           |||
+        // offsets   012345678
+
+        var result = service.getDiff(lData, rData);
 
         assertEquals(DIFFERENT, result.result());
-
-        // some-data
-        // |||
-        // cene-data
         assertEquals(1, result.differences().size());
+        // som >> cen
         assertEquals(0L, result.differences().get(0).offset());
         assertEquals(3L, result.differences().get(0).length());
     }
 
     @Test
-    @DisplayName("should return differences at the end")
-    public void shouldReturnChangesAtEnd() {
-        var result = service.getDiff("some-data", "some-yolo");
+    @DisplayName("should return difference at the end")
+    public void shouldReturnDifferenceAtEnd() {
+        var lData = "some-data";
+        //                ||||
+        var rData = "some-yolo";
+        //                ||||
+        // offsets   012345678
+
+        var result = service.getDiff(lData, rData);
 
         assertEquals(DIFFERENT, result.result());
-        // some-data
-        //      ||||
-        // some-yolo
         assertEquals(1, result.differences().size());
+        // data >> yolo
         assertEquals(5L, result.differences().get(0).offset());
         assertEquals(4L, result.differences().get(0).length());
     }
 
     @Test
-    @DisplayName("should return differences in the middle")
-    public void shouldReturnChangesInMiddle() {
-        var result = service.getDiff("some-data", "solo:beta");
+    @DisplayName("should return difference in the middle")
+    public void shouldReturnDifferenceInMiddle() {
+        var lData = "some-data";
+        //             |||||
+        var rData = "solo:beta";
+        //             |||||
+        // offsets   012345678
+
+        var result = service.getDiff(lData, rData);
 
         assertEquals(DIFFERENT, result.result());
-        // some-data
-        //   |||||
-        // solo:beta
         assertEquals(1, result.differences().size());
+        // me-da >> lo:be
         assertEquals(2L, result.differences().get(0).offset());
         assertEquals(5L, result.differences().get(0).length());
     }
 
     @Test
-    @DisplayName("should return multiple differences")
-    public void shouldReturnMultipleChanges() {
-        var result = service.getDiff("some-data", "iota-bela");
+    @DisplayName("should return difference in the entire string")
+    public void shouldReturnDifferenceInEntireString() {
+        var lData = "some-data";
+        //           |||||||||
+        var rData = "atad_emos";
+        //           |||||||||
+        // offsets   012345678
+
+        var result = service.getDiff(lData, rData);
 
         assertEquals(DIFFERENT, result.result());
-        // some-data
-        // | || |||
-        // iota-bela
+        assertEquals(1, result.differences().size());
+        assertEquals(0L, result.differences().get(0).offset());
+        assertEquals(9L, result.differences().get(0).length());
+    }
+
+    @Test
+    @DisplayName("should return multiple differences")
+    public void shouldReturnMultipleChanges() {
+        var lData = "some-data";
+        //           | || |||
+        var rData = "iota-bela";
+        //           | || |||
+        // offsets   012345678
+
+        var result = service.getDiff(lData, rData);
+
+        assertEquals(DIFFERENT, result.result());
         assertEquals(3, result.differences().size());
+        // s >> i
         assertEquals(0L, result.differences().get(0).offset());
         assertEquals(1L, result.differences().get(0).length());
+        // me >> ta
         assertEquals(2L, result.differences().get(1).offset());
         assertEquals(2L, result.differences().get(1).length());
+        // dat >> bel
         assertEquals(5L, result.differences().get(2).offset());
         assertEquals(3L, result.differences().get(2).length());
     }
